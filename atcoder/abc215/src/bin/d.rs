a2c7778406102560f154bd83acbc1a662cd2cbc2
urawa72 <urawa72@gmail.com>
2021-10-01 20:51:12 +0900

    atcoder abc215 d

--- atcoder/abc215/src/bin/d.rs
+++ atcoder/abc215/src/bin/d.rs
@@ -1,6 +1,40 @@
-use proconio::{input, fastout};
+use proconio::{fastout, input};
+use std::collections::HashSet;
+
+fn factorize(n: i32) -> Vec<i32> {
+    let mut i = 1;
+    let mut v = vec![];
+    while i * i <= n {
+        if n % i == 0 {
+            v.push(i);
+            v.push(n / i);
+        }
+        i += 1;
+    }
+    v
+}
 
 #[fastout]
 fn main() {
-    todo!();
+    input! { n: i32, m: i32, a: [i32; n] }
+
+    let hs = (0..n as usize)
+        .into_iter()
+        .flat_map(|i| factorize(a[i]))
+        .collect::<HashSet<i32>>();
+    let ans = (1..(m + 1))
+        .filter(|&i| {
+            let tmp = factorize(i).into_iter().collect::<HashSet<i32>>();
+            tmp.intersection(&hs).count() == 1
+        })
+        .collect::<Vec<i32>>();
+
+    println!("{}", ans.len());
+    println!(
+        "{}",
+        ans.into_iter()
+            .map(|i| i.to_string())
+            .collect::<Vec<String>>()
+            .join("\n")
+    )
 }
