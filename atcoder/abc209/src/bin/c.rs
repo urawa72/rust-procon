04f56fe6afbe41f966cf2e8da7455ecec44239c5
urawa72 <urawa72@gmail.com>
2021-10-07 21:20:27 +0900

    atcoder abc209 c

--- atcoder/abc209/src/bin/c.rs
+++ atcoder/abc209/src/bin/c.rs
@@ -2,5 +2,12 @@ use proconio::{input, fastout};
 
 #[fastout]
 fn main() {
-    todo!();
+    input! { n: usize, mut c: [usize; n] }
+    c.sort();
+    let mut ans = 1;
+    for i in 0..n {
+        ans *= std::cmp::max(0, c[i] - i);
+        ans %= 1000000007;
+    }
+    println!("{}", ans);
 }
