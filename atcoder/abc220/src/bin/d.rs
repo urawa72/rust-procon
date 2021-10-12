1665a97b86fbc838780cea90f2c663e898e9c6e5
urawa72 <urawa72@gmail.com>
2021-10-12 21:53:46 +0900

    atcoder abc220 d

--- atcoder/abc220/src/bin/d.rs
+++ atcoder/abc220/src/bin/d.rs
@@ -2,5 +2,19 @@ use proconio::{input, fastout};
 
 #[fastout]
 fn main() {
-    todo!();
+    input! { n: usize, a: [usize; n] }
+    const M: usize = 998244353;
+    let mut dp = vec![vec![0_usize; 10]; n];
+
+    dp[0][a[0]] = 1;
+    for i in 0..n - 1 {
+        for j in 0..10 {
+            dp[i + 1][(j + a[i + 1]) % 10] += dp[i][j] % M;
+            dp[i + 1][(j * a[i + 1]) % 10] += dp[i][j] % M;
+        }
+    }
+
+    for i in 0..10 {
+        println!("{}", dp[n - 1][i] % M);
+    }
 }
