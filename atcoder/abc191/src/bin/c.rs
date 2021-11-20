b53a919b96d55e4e96cc3d5f301f6bf5c74a8b81
urawa72 <urawa72@gmail.com>
2021-11-20 14:00:11 +0900

    atcoder abc191 c

--- atcoder/abc191/src/bin/c.rs
+++ atcoder/abc191/src/bin/c.rs
@@ -7,5 +7,28 @@ const MOD: usize = 1_000_000_007;
 
 #[fastout]
 fn main() {
-    todo!();
+    input! { h: usize, w: usize, s: [Chars; h] }
+
+    let mut ans = 0;
+    for i in 0..h - 1 {
+        for j in 0..w - 1 {
+            let mut cnt = 0;
+            if s[i][j] == '#' {
+                cnt += 1;
+            }
+            if s[i + 1][j] == '#' {
+                cnt += 1;
+            }
+            if s[i][j + 1] == '#' {
+                cnt += 1;
+            }
+            if s[i + 1][j + 1] == '#' {
+                cnt += 1;
+            }
+            if cnt == 1 || cnt == 3 {
+                ans += 1;
+            }
+        }
+    }
+    println!("{}", ans);
 }
