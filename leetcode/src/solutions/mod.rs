af2beb2e49fc50e18d46cfacf21a6145e183c1c4
urawa72 <urawa72@gmail.com>
2021-11-28 09:27:52 +0900

    leetcode s0070

--- leetcode/src/solutions/mod.rs
+++ leetcode/src/solutions/mod.rs
@@ -1,2 +1,3 @@
 pub mod s0002_add_to_numbers;
 pub mod s0069_sqrtx;
+pub mod s0070_climbing_stairs;
---
+++ leetcode/src/solutions/s0070_climbing_stairs.rs
@@ -0,0 +1,39 @@
+pub struct Solution {}
+
+impl Solution {
+    pub fn climb_stairs(n: i32) -> i32 {
+        let mut dp = vec![0i32; (n + 10) as usize];
+        dp[0] = 1;
+        for i in 0..n {
+            let i = i as usize;
+            dp[i + 1] += dp[i];
+            dp[i + 2] += dp[i];
+        }
+        return dp[n as usize];
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn s0070_case1() {
+        assert_eq!(2, Solution::climb_stairs(2));
+    }
+
+    #[test]
+    fn s0070_case2() {
+        assert_eq!(3, Solution::climb_stairs(3));
+    }
+
+    #[test]
+    fn s0070_case3() {
+        assert_eq!(17711, Solution::climb_stairs(21));
+    }
+
+    #[test]
+    fn s0070_case4() {
+        assert_eq!(1836311903, Solution::climb_stairs(45));
+    }
+}
