afcedf5e8d4586ee273e81a6fa5662eeec767085
urawa72 <urawa72@gmail.com>
2021-12-03 22:09:37 +0900

    leetcode s0088

--- leetcode/src/solutions/mod.rs
+++ leetcode/src/solutions/mod.rs
@@ -2,3 +2,4 @@ pub mod s0002_add_to_numbers;
 pub mod s0069_sqrtx;
 pub mod s0070_climbing_stairs;
 pub mod s0083_remove_deplicates_from_sorted_list;
+pub mod s0088_merge_sorted_array;
---
+++ leetcode/src/solutions/s0088_merge_sorted_array.rs
@@ -0,0 +1,74 @@
+struct Solution {}
+
+impl Solution {
+    pub fn merge(nums1: &mut Vec<i32>, m: i32, nums2: &mut Vec<i32>, n: i32) {
+        let mut i = (m + n) as usize - 1;
+        let mut m = m as usize;
+        let mut n = n as usize;
+
+        while 0 < m && 0 < n {
+            if nums2[n - 1] <= nums1[m - 1] {
+                nums1[i] = nums1[m - 1];
+                m -= 1;
+            } else {
+                nums1[i] = nums2[n - 1];
+                n -= 1;
+            }
+            i -= 1;
+        }
+
+        while 0 < n {
+            nums1[i] = nums2[n - 1];
+            if n == 0 {
+                break;
+            }
+            n -= 1;
+            i -= 1;
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn s0088_case1() {
+        let mut nums1 = vec![1, 2, 3, 0, 0, 0];
+        let mut nums2 = vec![2, 5, 6];
+        Solution::merge(
+            &mut nums1,
+            3,
+            &mut nums2,
+            3
+        );
+        assert_eq!(nums1, vec![1, 2, 2, 3, 5, 6]);
+    }
+
+    #[test]
+    fn s0088_case2() {
+        let mut nums1 = vec![1];
+        let mut nums2 = vec![];
+        Solution::merge(
+            &mut nums1,
+            1,
+            &mut nums2,
+            0
+        );
+        assert_eq!(nums1, vec![1]);
+    }
+
+    #[ignore = "Failed"]
+    #[test]
+    fn s0088_case3() {
+        let mut nums1 = vec![0];
+        let mut nums2 = vec![1];
+        Solution::merge(
+            &mut nums1,
+            0,
+            &mut nums2,
+            1
+        );
+        assert_eq!(nums1, vec![1]);
+    }
+}
